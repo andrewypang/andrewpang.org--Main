02d08247a66b3633c64e6baf5ff5e4b7c5375d0c
Andrew Pang <andrew.y.pang@gmail.com>
2020-06-01 23:41:45 -0700

    padding for scrollspy

--- dist/js/header-scrollspy.js
+++ dist/js/header-scrollspy.js
@@ -4,6 +4,6 @@ var header = document.querySelector('#site-nav');
 // Initialize Gumshoe
 var spy = new Gumshoe('#site-nav-list a', {
     offset: function () {
-        return header.getBoundingClientRect().height + 100;
+        return header.getBoundingClientRect().height;
     }
 });
